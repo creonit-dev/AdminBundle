5d2699d37c09311b7d9c7f58bcf44cd37132af77
P.Bondarovich <p.bondarovich@gmail.com>
2018-08-06 15:45:03 +0700

    Подключение модулей и плагинов с учетом приоритета.

--- src/DependencyInjection/Compiler/ModulePass.php
+++ src/DependencyInjection/Compiler/ModulePass.php
@@ -2,20 +2,21 @@
 
 namespace Creonit\AdminBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-class ModulePass implements CompilerPassInterface {
+class ModulePass implements CompilerPassInterface
+{
+    use PriorityTaggedServiceTrait;
 
     public function process(ContainerBuilder $container)
     {
+        $modulesIds = $this->findAndSortTaggedServices('creonit_admin.module', $container);
 
-        $modulesIds = $container->findTaggedServiceIds('creonit_admin.module');
-
-        foreach ($modulesIds as $moduleId => $options) {
-            $container->getDefinition('creonit_admin')->addMethodCall('addModule', [new Reference($moduleId), $options]);
+        foreach ($modulesIds as $moduleId) {
+            $container->getDefinition('creonit_admin')->addMethodCall('addModule', [new Reference((string) $moduleId)]);
         }
-        
     }
 } 
--- src/DependencyInjection/Compiler/PluginPass.php
+++ src/DependencyInjection/Compiler/PluginPass.php
@@ -2,23 +2,23 @@
 
 namespace Creonit\AdminBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 class PluginPass implements CompilerPassInterface {
 
+    use PriorityTaggedServiceTrait;
+
     public function process(ContainerBuilder $container)
     {
+        $pluginsIds = $this->findAndSortTaggedServices('creonit_admin.plugin', $container);
 
-        $pluginsIds = $container->findTaggedServiceIds('creonit_admin.plugin');
-
-        foreach ($pluginsIds as $pluginId => $options) {
-            $container->getDefinition('creonit_admin')->addMethodCall('addPlugin', [new Reference($pluginId), $options]);
+        foreach ($pluginsIds as $pluginId) {
+            $container->getDefinition('creonit_admin')->addMethodCall('addPlugin', [new Reference((string) $pluginId)]);
         }
 
         $container->getDefinition('creonit_admin')->addMethodCall('initialize');
-
-
     }
 } 
